50c8973ce2cdca0df774656736098cb02dfd1642
Lionmak <starshinov.ilya@yandex.ru>
2025-07-19 19:55:37 +0300

    added data eviction in the cache

--- cache/cache.go
+++ cache/cache.go
@@ -1,6 +1,7 @@
 package cache
 
 import (
+	"container/list"
 	"sync"
 )
 
@@ -13,14 +14,23 @@ type Interface interface {
 // Не меняйте названия структуры и название метода создания экземпляра Cache, иначе не будут проходить тесты
 
 type Cache struct {
-	data map[string]string
-	mu   sync.RWMutex
+	capacity int // Максимальное количество элементов
+	data     map[string]*list.Element
+	list     *list.List // Двусвязный список для реализации вытеснения LRU
+	mu       sync.RWMutex
+}
+
+type cacheEntry struct {
+	key   string
+	value string
 }
 
 // NewCache создаёт и возвращает новый экземпляр Cache.
-func NewCache() Interface {
+func NewCache(capacity int) Interface {
 	return &Cache{
-		data: make(map[string]string),
+		capacity: capacity,
+		data:     make(map[string]*list.Element),
+		list:     list.New(),
 	}
 }
 
@@ -28,14 +38,37 @@ func (c *Cache) Set(k, v string) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	c.data[k] = v
+	// Если ключ уже существует - обновляем значение и перемещаем в начало
+	if elem, ok := c.data[k]; ok {
+		c.list.MoveToFront(elem)
+		elem.Value.(*cacheEntry).value = v
+		return
+	}
+
+	// Добавляем новый элемент
+	elem := c.list.PushFront(&cacheEntry{k, v})
+	c.data[k] = elem
+
+	// Если превысили capacity - удаляем самый старый элемент
+	if c.list.Len() > c.capacity {
+		oldest := c.list.Back()
+		if oldest != nil {
+			delete(c.data, oldest.Value.(*cacheEntry).key)
+			c.list.Remove(oldest)
+		}
+	}
 }
 
 func (c *Cache) Get(k string) (v string, ok bool) {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
 
-	v, ok = c.data[k]
-	return v, ok
+	if elem, ok := c.data[k]; ok {
+		// Перемещаем элемент в начало (последний использованный)
+		c.list.MoveToFront(elem)
+		return elem.Value.(*cacheEntry).value, true
+	}
+
+	return "", false
 
 }
