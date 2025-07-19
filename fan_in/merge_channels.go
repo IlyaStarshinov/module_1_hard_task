f42b7f8fe6a19d967d23bdd6c06e4072fb515f12
Lionmak <starshinov.ilya@yandex.ru>
2025-07-19 15:37:15 +0300

    First commit

--- fan_in/merge_channels.go
+++ fan_in/merge_channels.go
@@ -1,9 +1,33 @@
 package fan_in
 
+import (
+	"log"
+	"sync"
+)
+
 // MergeChannels - принимает несколько каналов на вход и объединяет их в один
 // Fan-in и merge channels синонимы
 func MergeChannels(channels ...<-chan int) <-chan int {
-	// TODO: ваш код
+	out := make(chan int)
+	var wg sync.WaitGroup
 
-	panic("implement me")
+	output := func(ch <-chan int, workerID int) {
+		defer wg.Done()
+		for val := range ch {
+			log.Printf("Gorutina №%d processed the value of %d", workerID, val)
+			out <- val
+		}
+	}
+
+	wg.Add(len(channels))
+	for id, ch := range channels {
+		go output(ch, id)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
 }
--- generate_password/generate_password.go
+++ generate_password/generate_password.go
@@ -1,10 +1,25 @@
 package generate_password
 
+import (
+	"crypto/rand"
+	"math/big"
+)
+
 // letters - список допустимых символов в пароле
 const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 func GeneratePassword(n int) (string, error) {
-	// TODO: ваш код
+	password := make([]byte, n)
 
-	panic("implement me")
+	for i := range password {
+		val, err := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
+		if err != nil {
+			return "", err
+		}
+		idx := int(val.Int64())
+
+		password[i] = letters[idx]
+	}
+
+	return string(password), nil
 }
